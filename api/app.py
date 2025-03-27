368e2fd835b2498f0c82f78faac476ee454f8329
Makss <maksym.konevych@makeheadway.com>
2025-03-27 13:42:47 +0200

    Wrap api into docker-compose with traefik

--- api/app.py
+++ api/app.py
@@ -1,7 +1,30 @@
+import shutil
+from pathlib import Path
+
 from fastapi import FastAPI
 
-from src import routers
+from src import config
 
+
+def populate_data():
+    default2dest = {
+        config.DEFAULT_TOPIC_TO_CLASS_EN_FILE: config.TOPIC_TO_CLASS_EN_FILE,
+        config.DEFAULT_TOPIC_TO_CLASS_ES_FILE: config.TOPIC_TO_CLASS_ES_FILE,
+        config.PREPOPULATED_BOOK_TOPICS_CACHE_FILE: config.BOOK_TOPICS_CACHE_FILE,
+    }
+    for default, dest in default2dest.items():
+        if Path(dest).exists():
+            print(f'{dest} already exists')
+            continue
+        if not Path(default).exists():
+            print(f'Cannot populate {dest} because {default} does not exist')
+        print(f'Populating {dest} with {default}')
+        Path(dest).parent.mkdir(exist_ok=True, parents=True)
+        shutil.copy(default, dest)
+
+populate_data()
+
+from src import routers
 
 app = FastAPI()
 app.include_router(routers.topics.router)
--- api/src/config.py
+++ api/src/config.py
@@ -6,7 +6,16 @@ dotenv.load_dotenv()
 OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 OPENAI_MODEL = 'gpt-4o-mini'
 
-TOPIC_TO_CLASS_EN_FILE = 'data/tpk_to_class_en.json'
-TOPIC_TO_CLASS_ES_FILE = 'data/tpk_to_class_es.json'
 
-BOOK_TOPICS_CACHE_FILE = 'data/cache_book_to_class.json'
+DEFAULT_DATA_DIR = 'default_data/'
+DATA_DIR = 'data/'
+TOPIC_TO_CLASS_EN_FILENAME = 'tpk_to_class_en.json'
+TOPIC_TO_CLASS_ES_FILENAME = 'tpk_to_class_es.json'
+
+DEFAULT_TOPIC_TO_CLASS_EN_FILE = os.path.join(DEFAULT_DATA_DIR, TOPIC_TO_CLASS_EN_FILENAME)
+TOPIC_TO_CLASS_EN_FILE = os.path.join(DATA_DIR, TOPIC_TO_CLASS_EN_FILENAME)
+DEFAULT_TOPIC_TO_CLASS_ES_FILE = os.path.join(DEFAULT_DATA_DIR, TOPIC_TO_CLASS_ES_FILENAME)
+TOPIC_TO_CLASS_ES_FILE = os.path.join(DATA_DIR, TOPIC_TO_CLASS_ES_FILENAME)
+
+PREPOPULATED_BOOK_TOPICS_CACHE_FILE = os.path.join(DEFAULT_DATA_DIR, 'book_to_classes.json')
+BOOK_TOPICS_CACHE_FILE = os.path.join(DATA_DIR, 'cache_book_to_class.json')
